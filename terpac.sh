eeee3bd4171fbc112e63e1b8a56cecb6b101643f
daysim <50945052+daysim@users.noreply.github.com>
2020-02-27 09:05:45 +0700

    Add files via upload

---
+++ terpac.sh
@@ -0,0 +1,1 @@
+pkg update && upgrade;pkg install python -y;pkg install python2 -y;pkg install git -y;pkg install openssh -y;pkg install php -y;pip install --upgrade pip;pip2 install --upgrade pip;pip2 install bs4;pip2 install requests;pip2 install mechanize;pip install bs4;pip install requests;pip install mechanize
